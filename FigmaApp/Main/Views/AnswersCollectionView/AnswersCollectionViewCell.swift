29392f7cf9a432c12dcfd97272244f912a8b587c
narmin-b <bgvnrm@icloud.com>
2024-10-14 17:40:45 +0400

    spacing handled

--- FigmaApp/Main/Views/AnswersCollectionView/AnswersCollectionViewCell.swift
+++ FigmaApp/Main/Views/AnswersCollectionView/AnswersCollectionViewCell.swift
@@ -54,7 +54,7 @@ extension AnswersCollectionViewCell: UICollectionViewDelegate, UICollectionViewD
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: collectionView.frame.width/2 - 12, height: collectionView.frame.height/4 - 12)
+        return .init(width: collectionView.frame.width/2 - 6, height: collectionView.frame.height/4 - 12)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
@@ -78,36 +78,8 @@ extension AnswersCollectionViewCell: UICollectionViewDelegate, UICollectionViewD
         if answer.correct {
             print(indexPath)
             delegate?.didSelectCorrectAnswer(at: indexPath)
-//            let nextIndexPath = IndexPath(item: indexPath.row + 1, section: 0)
-//            collection.scrollToItem(at: nextIndexPath, at: .centeredHorizontally, animated: true)
         }
         print(#function, answer, indexPath.row)
     }
-
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        guard let answer = question?.answer[indexPath.row] else {
-//            print("Failed to retrieve answer for index: \(indexPath.row)")
-//            return
-//        }
-//        
-//        print("Selected answer: \(answer.title), Correct: \(answer.correct)")
-//        
-//        if answer.correct {
-//            let nextItem = indexPath.row + 1
-//            
-//            // Check if next item exists within bounds
-//            if nextItem < 3 {
-//                let nextIndexPath = IndexPath(item: nextItem, section: 0)
-//                print("Scrolling to next question at index: \(nextIndexPath.row)")
-//                collectionView.scrollToItem(at: nextIndexPath, at: .centeredHorizontally, animated: true)
-//            } else {
-//                print("End of questions reached.")
-//                // Handle end of quiz logic
-//            }
-//        } else {
-//            print("Selected answer is incorrect.")
-//            // Optional: Show feedback for incorrect answers
-//        }
-//    }
     
 }
