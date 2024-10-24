092adf244a90a3d226bfbee0305725d4304bd101
narmin-b <bgvnrm@icloud.com>
2024-10-24 09:47:24 +0400

    finished updates

---
+++ FigmaApp/Main/Views/ResultTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultTableCell.swift
+//  FigmaApp
+//
+//  Created by Narmin Baghirova on 21.10.24.
+//
+
+import UIKit
+
+class ResultTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
